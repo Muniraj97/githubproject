aeb04850c5fe98f4be819c12484bad869fadefc3
Muniraj97 <muniram14meiet92@gmail.com>
2025-03-11 23:29:28 +0530

    muni

---
+++ src/org/pac/Employee.java
@@ -0,0 +1,17 @@
+package org.pac;
+
+public class Employee {
+	public int a=10;
+	private void empName() {
+		a=50;
+		
+	}
+public static void main(String[] args) {
+	Employee w=new Employee();
+	System.out.println(w.a);
+	w.empName();
+	System.out.println(w.a);
+	
+	w.empName();
+	}
+}
